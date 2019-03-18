3601ecdac7176986151cc152e9144d5c0d18f3e4
Christopher Beckham <chrispy645@gmail.com>
2019-03-18 12:03:20 -0400

    init

---
+++ architectures/arch_fashiongen.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from .shared import networks
+from functools import partial
+
+def get_network():
+    n_channels = 3
+    ngf = 64
+    norm_layer = partial(nn.InstanceNorm2d, affine=True)
+    gen = networks.ResnetEncoderDecoder(input_nc=n_channels,
+                                        output_nc=n_channels,
+                                        ngf=ngf,
+                                        n_blocks=4,
+                                        n_downsampling=4,
+                                        norm_layer=norm_layer)
+    return gen
+
+if __name__ == '__main__':
+    print("ooo")
